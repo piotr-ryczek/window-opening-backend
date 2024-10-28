7ebe531e0948b3601cd99bbb29f8607cb761b6e2
Piotr Ryczek <piotrryczek@gmail.com>
2024-10-29 00:35:43 +0100

    Under dev

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './logger.interceptor';
 
 // Copying node_modules to myDevil
 // scp -r ./node_modules/ Nero12@panel41.mydevil.net:~/domains/window-opening.nero12.usermd.net/public_nodejs
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
